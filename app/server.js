1e6fe1177d7819ceaa7b019fbbad51b792dcec07
Alvaro Paco <alvaropaconeto@gmail.com>
2016-04-28 19:05:59 -0300

    Code climate integration

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 var express   = require("express"),
     app       = express(),
     Sequelize = require("sequelize"),
-    models    = require(__dirname + "/models");
+    path      = require("path"),
+    models    = require(path.join(__dirname, "/models"));
 
 app.listen(3000);
